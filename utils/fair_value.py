8de6ac2a83d15f3f0b212451d61aa617ca6b2e71
Fares Ben Slama <fares.benslama@esprit.tn>
2026-03-30 00:56:59 +0000

    first commit

--- utils/fair_value.py
+++ utils/fair_value.py
@@ -1,18 +1,19 @@
 import math
 
 def graham_fair_value(eps, bvps):
-    if not eps or not bvps:
+    if not eps or not bvps or eps < 0 or bvps < 0:
         return None
     try:
+        # Graham's formula: sqrt(22.5 * EPS * BVPS)
         return math.sqrt(22.5 * eps * bvps)
     except:
         return None
 
-
 def margin_of_safety(price, fair_value):
-    if not price or not fair_value:
+    if not price or not fair_value or fair_value <= 0:
         return None
     try:
-        return (fair_value - price) / fair_value
+        # returns as a percentage (e.g., 37.3)
+        return round(((fair_value - price) / fair_value) * 100, 2)
     except:
         return None
